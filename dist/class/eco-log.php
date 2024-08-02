30850af1292f8e0f80df06f8e6bcf94f5a4d6d90
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2024-08-01 22:08:57 -0300

    Defining methods
    In this new update, we are starting to define our first methods that will be used to manipulate the generated log files. The entire file manipulation flow will be defined by the ECOLog class.

--- dist/class/eco-log.php
+++ dist/class/eco-log.php
@@ -1,8 +1,45 @@
 <?php
 
 class ECOLog {
+	
+	function Init(){
+
+	}
+
+	function Close(){
+
+	}
+
+    /**
+ 	* @param int $register_id The unique identifier of the log entry.
+ 	* @param string $title The title or description of the event.
+ 	* @param string $hostname The hostname or server where the event occurred.
+ 	* @param string $ip_address The IP address associated with the event.
+ 	* @param string $severity_level The severity level of the event (e.g., INFO, WARNING, ERROR).
+ 	* @param string $source The source of the event (e.g., software component, service, application).
+ 	* @param string $date The date and time when the event occurred.
+ 	* @param string $path_file The path to the file where the log will be saved.
+ 	* @return void
+ 	**/
+    function Save($register_id, $title, $hostname, $ip_address, $severity_level, $source, $date, $path_file){
+
+    }
     
+    /**
+ 	* @param string $path_file The path to the file where the log will be saved.
+ 	* @return void
+ 	**/
+    function Delete($path_file){
+
+    }
     
+    /**
+ 	* @param string $path_file The path to the file where the log will be saved.
+ 	* @return void
+ 	**/
+    function Clear($path_file){
+
+    }
 
 }
 
